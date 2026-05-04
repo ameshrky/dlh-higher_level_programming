683ae014561a8db9b3ae770f40f4f11f3ee42750
Antwan Meshrky <antwan.meshrky@ai.dlh.lu>
2026-05-04 13:33:04 +0000

    code stype fix task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print("{a} + {b} = {c}".format(a=a, b=b, c=add(a,b)))
+    print("{a} + {b} = {c}".format(a=a, b=b, c=add(a, b)))
