1ab085760ac4c00f6a9c385fd6ceecbe055c89b0
Antwan Meshrky <antwan.meshrky@ai.dlh.lu>
2026-05-10 15:47:36 +0000

    task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        print("")
+        return None
+    for row in range(len(matrix)):
+        for column in range(len(matrix[row])):
+            if column == len(matrix[row]) - 1:
+                print("{:d}".format(matrix[row][column]))
+            else:
+                print("{:d} ".format(matrix[row][column]),end ="")
