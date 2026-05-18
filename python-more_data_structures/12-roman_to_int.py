1d868ba10dee2f13288f6e8c18582590ab4e279e
Antwan Meshrky <antoinemagdi@gmail.com>
2026-05-18 22:51:01 +0000

    task 11 logic modified

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -22,27 +22,27 @@ def roman_to_int(roman_string):
     i = len(roman_string) - 1
     while i >= 0:
         if roman_string[i] == 'X':
-            if roman_string[i-1] == 'I':
+            if i > 0 and roman_string[i-1] == 'I':
                 roman_int += roman_int_map.get('IX')
                 i -= 2
                 continue
         if roman_string[i] == 'L':
-            if roman_string[i-1] == 'X':
+            if i > 0 and roman_string[i-1] == 'X':
                 roman_int += roman_int_map.get('XL')
                 i -= 2
                 continue
         if roman_string[i] == 'C':
-            if roman_string[i-1] == 'X':
+            if i > 0 and roman_string[i-1] == 'X':
                 roman_int += roman_int_map.get('XC')
                 i -= 2
                 continue
         if roman_string[i] == 'D':
-            if roman_string[i-1] == 'C':
+            if i > 0 and roman_string[i-1] == 'C':
                 roman_int += roman_int_map.get('CD')
                 i -= 2
                 continue
         if roman_string[i] == 'M':
-            if roman_string[i-1] == 'C':
+            if i > 0 and roman_string[i-1] == 'C':
                 roman_int += roman_int_map.get('CM')
                 i -= 2
                 continue
