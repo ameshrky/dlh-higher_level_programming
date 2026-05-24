3a8455dab4e27af7d4acd3c6a4f2447d12a34fa2
Antwan Meshrky <antoinemagdi@gmail.com>
2026-05-24 21:43:42 +0000

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module shows an example for how to create a class"""
+
+
+class Square:
+    """This class describes the blueprint of a square"""
+
+    def __init__(self, size=0):
+        """Initialize a square with a size. size here is a private field/attribute"""
+        self.__size = size
