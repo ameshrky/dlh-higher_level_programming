5f97220aa2c7c2bda8aaddbd4fa261c46e563501
Antwan Meshrky <antoinemagdi@gmail.com>
2026-05-24 23:39:13 +0000

    task 1 submission

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This modules address the Python - more classes project."""
+
+
+class Rectangle:
+    """This class is the blue print of Rectangle objects."""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This modules address the Python - more classes project."""
+
+
+class Rectangle:
+    """This class is the blue print of Rectangle objects."""
+
+    pass
