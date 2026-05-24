62f2feab64f8ab0f1b476509f094b372cb8a1d64
Antwan Meshrky <antoinemagdi@gmail.com>
2026-05-24 23:10:17 +0000

    task 6 modified

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -8,18 +8,8 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a square with a size. size here is a private field."""
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-
-        if size < 0:
-            raise ValueError("size must be >= 0")
-
-        self.__size = size
-
-        if position[0] < 0 or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -45,7 +35,11 @@ class Square:
     @position.setter
     def position(self, value):
         """set the position of the square."""
-        if value[0] < 0 or value[1] < 0:
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not isinstance(value[0], int) or
+                not isinstance(value[1], int) or
+                value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
 
         self.__position = (value[0], value[1])
