663c966507c7d3fcb11bc72d11c0905111e9db1e
Antwan Meshrky <antoinemagdi@gmail.com>
2026-05-24 21:30:18 +0000

    task 0 solution

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module shows an example for how to create a class"""
+
+
+class Square:
+    """This class describes the blueprint of a square"""
+
+    def __init__(self):
+        pass
