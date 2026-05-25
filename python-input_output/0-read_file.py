7f66733eb94a7dd84a9846cccce65aa6d4b65f8f
Antwan Meshrky <antoinemagdi@gmail.com>
2026-05-25 22:14:10 +0000

    task 0 fixed

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
+    """Reads a UTF8 text file and prints it to stdout."""
     with open(filename, encoding="utf-8") as myFile:
         x = myFile.read()
         print(x)
